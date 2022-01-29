f39beef855db30557f739280465709da69a620a8
zenon-provalidator <zenon@provalidator.com>
2022-01-30 01:21:50 +0900

    api

--- app/api.py
+++ app/api.py
@@ -1,5 +1,7 @@
 #chain api ( rpc , lcd .. )
 import config
+import requests
+import json
 
 class Api :
     cfg = ''
@@ -7,11 +9,22 @@ class Api :
     def __init__(self):
         self.cfg = config.Config()
 
-    def getRpcData(self):
-        osm = self.cfg.getValue("rpc","osmosis")
-        print(osm)
+    def getDataFromUrl(self, url):
+        data = requests.get(url)
+        return data.text
+    
+    def getRpcLatestBlockHeight(self, coinName):
+        url = self.cfg.getValue('rpc', coinName) + '/status'
+        jsonObj = json.loads(self.getDataFromUrl(url))
+        return jsonObj['result']['sync_info']['latest_block_height']
+    
+    def getExternRpcLatestBlockHeight(self, coinName):
+        url = self.cfg.getValue('rpc-extern', coinName) + '/status'
+        jsonObj = json.loads(self.getDataFromUrl(url))
+        return jsonObj['result']['sync_info']['latest_block_height']
+        
     
     
     
 api = Api()
-api.getRpcData()    
+print(api.getRpcLatestBlockHeight('osmosis'))
