6d0675cc1e0d8927ae426973258ddc2ef298c220
zenon-provalidator <zenon@provalidator.com>
2022-01-29 17:23:04 +0900

    config & api update

--- app/api.py
+++ app/api.py
@@ -1,1 +1,17 @@
 #chain api ( rpc , lcd .. )
+import config
+
+class Api :
+    cfg = ''
+    
+    def __init__(self):
+        self.cfg = config.Config()
+
+    def getRpcData(self):
+        osm = self.cfg.getValue("rpc","osmosis")
+        print(osm)
+    
+    
+    
+api = Api()
+api.getRpcData()    
